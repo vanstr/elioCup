f82d2565ea2763904c85595802fa32260fae7927
Ivan Mihnenok <imi@ambita.com>
2015-09-07 10:21:34 +0200

    Updated Readme

--- app/utils/HipChat.java
+++ app/utils/HipChat.java
@@ -14,7 +14,7 @@ public class HipChat {
 
   private static final String HIPCHAT_HOST = "https://api.hipchat.com";
   private static final String AUTH_TOKEN = "7xa08bQVnt5wXRnOe9DnvtGeBhxcHkKgpLuELwhQ";
-  public static final long CRONJOB_INTERVAL_SECONDS = 10L;
+  public static final long CRONJOB_INTERVAL_SECONDS = 60L;
 
   // user_id 1154180, 1154277, 1282848
   public static void updateAllUsersAvailability() {
@@ -37,7 +37,7 @@ public class HipChat {
   }
 
   public static WSResponse getRequest(String url) {
-    WSResponse response = WS.url(url).get().get(2000L);
+    WSResponse response = WS.url(url).get().get(3000L);
 
     Logger.trace("send request. url: " + url + " response: " + response.getStatus());
     return response;
